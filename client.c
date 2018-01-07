3733bd01c930e94114b901c52338e598bfe1b68f
plutasnyy <kamil.plucinski97@gmail.com>
2018-01-07 18:26:53 +0100

    some things with comunicating

--- client.c
+++ client.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
+//IPC
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+//SLEEP
+#include <unistd.h>
+//strcpy
+#include <string.h>
+
+#define MSGPERM 0640    // msg queue permission
+
+struct message {
+    long type;
+    char* a;
+} msg;
 
 int main(int argc, char *argv[]) {
-    int i;
-
-    printf("The following arguments were passed to main(): ");
-    for (i = 1; i < argc; i++) printf("%s ", argv[i]);
-    printf("\n");
-
-    return 0;
+    int msqid = msgget(812355, MSGPERM|IPC_CREAT);
+    int result = msgsnd(msqid, &msg, sizeof(msg.a), 0);
+    char a[50];
+    int i = fork();
+    if (i==0) {
+        while(1) {
+            printf("Wiadomosc z serwera\n");
+            sleep(50);
+        }
+    }
+    else {
+        while(1){
+            printf("Podaj wiadomosc:");
+            sleep(50);
+            scanf("%s",&a);
+            printf("Komenda do wyslania: %s\n",a);
+        }
+    }
 }
--- server.c
+++ server.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+#include <errno.h>
+#define MSGPERM 0640    // msg queue permission
+
+struct message {
+    long type;
+    int a;
+} msg;
+
 
 int main(int argc, char *argv[])
 {
-    int i;
+    int msqid = msgget(812355, MSGPERM|IPC_CREAT|IPC_EXCL);
+    int result = msgrcv(msqid, &msg, sizeof(msg.a), 1, 0);
 
-    printf("The following arguments were passed to main(): ");
-    for(i=1; i<argc; i++) printf("%s ", argv[i]);
-    printf("\n");
-
+    if (result==-1){
+        printf("%s,Oh dear, something went wrong with read()! %s\n",errno, strerror(errno));
+    }
+    printf("Wiadomosc: %s \n",msg.text);
     return 0;
 }
