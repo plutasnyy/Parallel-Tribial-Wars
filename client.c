58a340ce2641cb897604165dc6c6b3899f300e1a
plutasnyy <kamil.plucinski97@gmail.com>
2018-01-15 16:19:16 +0100

    ingore

--- client.c
+++ client.c
@@ -5,9 +5,9 @@
 #include <unistd.h>
 #include <stdlib.h>  // for strtol
 #include <string.h>
-
+#include <stdbool.h>
 #define MSGPERM 0640    // msg queue permission
-int id;
+long id;
 /*
  * to run:
  * ./client id rw
@@ -21,24 +21,42 @@ struct Message {
 } msg;
 
 void sendMsg(char text[]){
-    printf("%s\n",text);
+    strcpy(msg.text,text);
+    printf("%s\n",msg.text);
+
 }
+bool is_correct_message(char text[]){
+    if (!strcmp(text,"connect")) return true;
+    return false;
+}
+
+
+
+
 void main_read(){
     printf("Information from server:\n");
 }
+
+
 
 void main_write(){
     char text[1024];
     while(1){
         scanf("%s",text);
-        sendMsg(text);
-        printf("%s\n",text);
+        if(is_correct_message(text)) sendMsg(text);
+        else printf("Incorrect message\n");
+        break;
     }
 }
+
+
+
+
+
+
 int main(int argc, char *argv[]) {
     id = strtol(argv[1], NULL, 10);
-    printf("your id: %d\n",id);
-    const int rw=strtol(argv[2], NULL, 10);
+    const long rw=strtol(argv[2], NULL, 10);
     if(rw==0)main_read();
     else if(rw==1)main_write();
     else printf("BAD ARGUMENT");
